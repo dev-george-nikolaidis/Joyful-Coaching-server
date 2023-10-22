d44be70d28c37d70f27adc145229480175394f7a
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2023-10-22 18:03:39 +0200

    add lazy routes loading

--- src/api/v1/indexRoutes.ts
+++ src/api/v1/indexRoutes.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import { createLazyRouter } from "express-lazy-router";
+
+const lazyLoad = createLazyRouter({
+	preload: process.env.NODE_ENV === "production",
+});
 const router = express.Router();
 
-import appointmentsRoutes from "./appointments/appointmentsRoutes";
-import contactRoutes from "./contact/contactRoutes";
-import usersRoutes from "./users/usersRoutes";
+router.use(
+	"/users",
+	lazyLoad(() => import("./users/usersRoutes"))
+);
 
-router.use("/users", usersRoutes);
-
-router.use("/appointments", appointmentsRoutes);
-router.use("/contact", contactRoutes);
+router.use(
+	"/appointments",
+	lazyLoad(() => import("./appointments/appointmentsRoutes"))
+);
+router.use(
+	"/contact",
+	lazyLoad(() => import("./contact/contactRoutes"))
+);
 
 export default router;
