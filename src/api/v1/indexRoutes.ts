b116f8630ab8a511c2ac4b8560ec4346c7520562
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2023-10-23 19:16:58 +0200

    add configs for deployment

--- src/api/v1/indexRoutes.ts
+++ src/api/v1/indexRoutes.ts
@@ -5,6 +5,9 @@ const lazyLoad = createLazyRouter({
 	preload: process.env.NODE_ENV === "production",
 });
 const router = express.Router();
+router.get("/welcome", (req, res) => {
+	res.send("Welcome");
+});
 
 router.use(
 	"/users",
