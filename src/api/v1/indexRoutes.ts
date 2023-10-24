f8f425a844626aaf709b51d352955f642ff98453
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2023-10-24 15:43:46 +0200

    fix jwt bug

--- src/api/v1/indexRoutes.ts
+++ src/api/v1/indexRoutes.ts
@@ -1,23 +1,32 @@
 import express from "express";
-import { createLazyRouter } from "express-lazy-router";
+// import { createLazyRouter } from "express-lazy-router";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const lazyLoad = createLazyRouter({
-	preload: process.env.NODE_ENV === "production",
-});
+// const lazyLoad = createLazyRouter({
+// 	preload: process.env.NODE_ENV === "production",
+// });
 const router = express.Router();
 
-router.use(
-	"/users",
-	lazyLoad(() => import("./users/usersRoutes"))
-);
+// router.use(
+// 	"/users",
+// 	lazyLoad(() => import("./users/usersRoutes"))
+// );
 
-router.use(
-	"/appointments",
-	lazyLoad(() => import("./appointments/appointmentsRoutes"))
-);
-router.use(
-	"/contact",
-	lazyLoad(() => import("./contact/contactRoutes"))
-);
+// router.use(
+// 	"/appointments",
+// 	lazyLoad(() => import("./appointments/appointmentsRoutes"))
+// );
+// router.use(
+// 	"/contact",
+// 	lazyLoad(() => import("./contact/contactRoutes"))
+// );
+import appointmentsRoutes from "./appointments/appointmentsRoutes";
+import contactRoutes from "./contact/contactRoutes";
+import usersRoutes from "./users/usersRoutes";
 
+router.use("/users", usersRoutes);
+
+router.use("/appointments", appointmentsRoutes);
+router.use("/contact", contactRoutes);
 export default router;
